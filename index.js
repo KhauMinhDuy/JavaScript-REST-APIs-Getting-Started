b116974492bb98c58b9fb2968123c43d6c0c5816
khauminhduy <khauminhduy@gmail.com>
2022-08-05 00:01:45 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+
+let pieRepo = require("./repos/pieRepo");
 
 let router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('Cherry pie');
-})
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    status: 200,
+    statusText: "OK",
+    message: "All pies retrieved",
+    data: pieRepo.get(),
+  });
+});
 
-app.use('/api/', router);
+app.use("/api/", router);
 
 const server = app.listen(5000, () => {
-    console.log('Node Server is running on http://localhost:5000/...');
-})
+  console.log("Node Server is running on http://localhost:5000/...");
+});

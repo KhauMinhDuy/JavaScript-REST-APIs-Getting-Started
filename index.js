a8345d22f934701c33c2504073e1b13409e51717
khauminhduy <khauminhduy@gmail.com>
2022-08-05 15:03:26 +0700

    update

--- index.js
+++ index.js
@@ -6,12 +6,50 @@ let pieRepo = require("./repos/pieRepo");
 let router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    status: 200,
-    statusText: "OK",
-    message: "All pies retrieved",
-    data: pieRepo.get(),
-  });
+  pieRepo.get(
+    (data) => {
+      res.status(200).json({
+        status: 200,
+        statusText: "OK",
+        message: "All pies retrieved",
+        data: data,
+      });
+    },
+    (err) => {
+      next(err);
+    }
+  );
+});
+
+router.get("/:id", (req, res, next) => {
+  let id = Number.parseInt(req.params.id);
+  pieRepo.getById(
+    id,
+    (data) => {
+      if(data) {
+        res.status(200).json({
+          'status': 200,
+          'statusText': "OK",
+          'message': "All pies retrieved",
+          'data': data,
+        });
+      } else {
+        res.status(404).json({
+          'status': 404,
+          'statusText': "Not Found",
+          'message': `The pie ${req.params.id} could not be found`,
+          'error': {
+            'code': 'NOT_FOUND',
+            'message': `The pie ${req.params.id} could not be found` 
+          },
+        });
+      }
+      
+    },
+    (err) => {
+      next(err);
+    }
+  );
 });
 
 app.use("/api/", router);
--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,10 +1,24 @@
+const { FILE } = require("dns");
+const fs = require("fs");
+const FILE_NAME = "./assets/pies.json";
 const pieRepo = {
-  get: function () {
-    return [
-      { id: 1, name: "Apple" },
-      { id: 2, name: "Cherry" },
-      { id: 3, name: "Peach" },
-    ];
+  get: function (resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  },
+  getById: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data).find((p) => p.id === id));
+      }
+    });
   },
 };
 

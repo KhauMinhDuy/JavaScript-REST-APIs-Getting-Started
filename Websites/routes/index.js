c9c910a3cb04852e75e2889dea307080f4ecc006
khauminhduy <khauminhduy@gmail.com>
2022-08-07 20:21:52 +0700

    update

--- Websites/routes/index.js
+++ Websites/routes/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const apiHelper = require("../helpers/apiHelper");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rest API Getting Started' });
+router.get("/", function (req, res, next) {
+  apiHelper.callApi("http://localhost:5000/api/").then((response) => {
+    console.log(response);
+    res.render("index", {
+      title: "Rest API Getting Started",
+      data: response.data,
+    });
+  }).catch(error => {
+    res.send(error);
+  });
+  // res.render("index", { title: "Rest  Getting Started" });
 });
 
 module.exports = router;
